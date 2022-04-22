df41848f089331da47d91b133e7b65f6ff1c8a43
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-04-23 01:02:34 +0200

    python script to generate image

---
+++ generate_image.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import numpy as np
+
+
+pixels = [
+   [(54, 54, 54), (232, 23, 93), (71, 71, 71), (168, 167, 167)],
+   [(204, 82, 122), (54, 54, 54), (168, 167, 167), (232, 23, 93)],
+   [(71, 71, 71), (168, 167, 167), (54, 54, 54), (204, 82, 122)],
+   [(168, 167, 167), (204, 82, 122), (232, 23, 93), (54, 54, 54)]
+]
+## TODO: update this to take in the appropriate structure
+
+
+# Convert the pixels into an array using numpy
+array = np.array(pixels, dtype=np.uint8)
+
+# Use PIL to create an image from the new array of pixels
+new_image = Image.fromarray(array)
+new_image.save('new.png')

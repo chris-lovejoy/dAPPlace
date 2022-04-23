4f3dacbf2d0ce588b212a3dcf4f728a98876777d
Joao Meyer <1994meyer@gmail.com>
2022-04-24 00:32:07 +0100

    Removed unused contract

--- hardhat/app/src/contracts.js
+++ hardhat/app/src/contracts.js
@@ -1,22 +1,8 @@
 import { ethers } from 'ethers'
 
 import CanvasABI from '../../artifacts/contracts/Canvas.sol/Canvas.json'
-import CounterABI from '../../artifacts/contracts/Counter.sol/Counter.json'
-import GreeterABI from '../../artifacts/contracts/Greeter.sol/Greeter.json'
 
 const provider = new ethers.providers.Web3Provider(window.ethereum)
-
-export const Greeter = new ethers.Contract(
-  '0x6de6baDcC399a836258fa92d91EbA0a02cC40eE2',
-  GreeterABI.abi,
-  provider
-)
-
-export const Counter = new ethers.Contract(
-  '0x1F78AC65DCF6F7B696B5175D7D5E516FE07636a9',
-  CounterABI.abi,
-  provider
-)
 
 export const Canvas = new ethers.Contract(
   '0xAAc0a62E090145330fce78771d699999Fa64654D',
